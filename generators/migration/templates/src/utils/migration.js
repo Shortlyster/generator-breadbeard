ef182538d0ca66991a4da75bf2cb4ed710519836
Nikolay Nemshilov <nemshilov@gmail.com>
2017-09-06 10:42:30 +1000

    Fixes the migrations mutex unlocks in case of migration failures

--- generators/migration/templates/src/utils/migration.js
+++ generators/migration/templates/src/utils/migration.js
@@ -9,7 +9,7 @@ const { slurp } = require('./module');
 const MUTEX_MIGRATION_NAME = 'migrations_running_mutex';
 
 const migrationFiles = slurp('migrations');
-const existingMigrations = Object.keys(migrationFiles).sort().map(name => {
+const existingMigrations = Object.keys(migrationFiles).sort().map((name) => {
   const { up, down } = migrationFiles[name];
   return { up, down, name: _.snakeCase(name) };
 });
@@ -18,7 +18,7 @@ exports.list = async () => {
   await Migration.ready();
   const records = await Migration.run();
 
-  return existingMigrations.map(migration => {
+  return existingMigrations.map((migration) => {
     const { name, up, down } = migration;
     const [applied] = records.filter(r => r.name === name);
     return { up, down, name, applied: !!applied };
@@ -47,47 +47,49 @@ exports.acquireMutex = async () => {
 
 exports.releaseMutex = async () => {
   const mutex = await exports.queryMutex();
-
-  if (!mutex) throw new Error('Mutex was not found');
-
-  return await mutex.delete();
+  return mutex && await mutex.delete();
 };
 
 exports.up = async (name) => {
   await exports.acquireMutex();
 
-  const migrations = await exports.list();
-  const pendingMigrations = migrations.filter(m =>
-    !m.applied && (name ? m.name === name : true)
-  );
+  try {
+    const migrations = await exports.list();
+    const pendingMigrations = migrations.filter(m =>
+      !m.applied && (name ? m.name === name : true)
+    );
 
-  if (pendingMigrations.length < 1) throw new Error('No migration files found');
+    if (pendingMigrations.length < 1) {
+      console.log('No pending migrations found');
+    }
 
-  for (let i = 0; i < pendingMigrations.length; i++) {
-    const { up, name } = pendingMigrations[i];
-    console.log('migrating', name);
-    await up();
-    await Migration.save({ name, date: new Date() });
+    for (const migration of pendingMigrations) {
+      console.log('Migrating', migration.name);
+      await migration.up();
+      await new Migration({ name: migration.name, date: new Date() }).save();
+    }
+  } finally {
+    await exports.releaseMutex();
   }
-
-  await exports.releaseMutex();
 };
 
 exports.down = async (name) => {
   await exports.acquireMutex();
 
-  const migrations = await exports.list();
-  const [migration] = migrations.filter(m => m.name === name);
+  try {
+    const migrations = await exports.list();
+    const [migration] = migrations.filter(m => m.name === name);
 
-  if (!migration) throw new Error('Could not find the migration');
-  if (!migration.applied) throw new Error('This migration was never applied');
-  if (!migration.down) throw new Error('This migration does not have a `down` option');
+    if (!migration) throw new Error('Could not find the migration');
+    if (!migration.applied) throw new Error('This migration was never applied');
+    if (!migration.down) throw new Error('This migration does not have a `down` option');
 
-  console.log('rolling back', name);
-  await migration.down();
+    console.log('Rolling back', name);
+    await migration.down();
 
-  const [record] = await Migration.filter({ name }).run();
-  if (record) await record.delete();
-
-  await exports.releaseMutex();
+    const [record] = await Migration.filter({ name }).run();
+    if (record) await record.delete();
+  } finally {
+    await exports.releaseMutex();
+  }
 };
