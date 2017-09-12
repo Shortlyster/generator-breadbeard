0f6a33ac7703a40c409b7d8a4eeabce542bc6686
Drew Showalter <maelstroswim@gmail.com>
2017-09-12 16:11:35 +1000

    :recycle: remove newrelic logging line.

--- generators/base/templates/src/index.js
+++ generators/base/templates/src/index.js
@@ -5,7 +5,6 @@ const routes = require('./routes');
 const logger = require('morgan');
 const { httpErrors } = require('./middleware');
 const { NODE_ENV } = require('../config');
-if (process.env.RUNTIME_ANALYTICS) require('newrelic'); // eslint-disable-line
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
